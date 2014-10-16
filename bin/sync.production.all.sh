7e937d8b4b93ff646955eea37f8fff5fdc05afd0
Matthäus Zloch <matthaeus.zloch@gmail.com>
2014-10-16 10:37:43 +0200

    sync all -> changed username for server ssh.yatimesheet.de

--- bin/sync.production.all.sh
+++ bin/sync.production.all.sh
@@ -1,2 +1,2 @@
 # syncing controllers
-rsync --delete -avze ssh app/routes.php app/controllers app/models app/views mindstackr.com@ssh.yatimesheet.de:/www/app/
+rsync --delete -avze ssh app/routes.php app/controllers app/models app/views yatimesheet.de@ssh.yatimesheet.de:/www/app/
