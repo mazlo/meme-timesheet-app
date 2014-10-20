36424c4895639874a155b2bf555b628faf4c78b0
Matthäus Zloch <matthaeus.zloch@gmail.com>
2014-10-20 22:32:33 +0200

    weekly summary by day, fixed: passing wrong date format

--- app/controllers/TisheetController.php
+++ app/controllers/TisheetController.php
@@ -182,12 +182,15 @@ class TisheetController extends BaseController
     */
     public function summaryOfLastWeekByDay( $day )
     {
+        $dayAsTime = strtotime( $day );
+
         // select day, time_spent, c.prefLabel from tisheets t join contexts c on t.context_id=c.id group by day, c.preflabel;
         $sum = DB::table( 'tisheets' )
             ->join( 'contexts', 'tisheets.context_id', '=', 'contexts.id' )
             ->select( 'tisheets.day', DB::raw( 'sum( tisheets.time_spent ) as time_spent' ), 'contexts.prefLabel' )
             ->where( 'tisheets.user_id', Auth::user()->id )
-            ->where( 'tisheets.day', '>', date( "Y-m-d", strtotime( $day ) - 60*60*24*7 ) )
+            ->where( 'tisheets.day', '>', date( 'Y-m-d', strtotime( '-1 week', $dayAsTime ) ) )
+            ->where( 'tisheets.day', '<=', date( 'Y-m-d', $dayAsTime ) )
             ->groupBy( 'tisheets.day' )
             ->groupBy( 'contexts.prefLabel' )
             ->get();
