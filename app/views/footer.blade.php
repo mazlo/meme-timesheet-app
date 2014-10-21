27a5d766d98edd0840b78035d7b8676627ab4d67
Matthäus Zloch <matthaeus.zloch@gmail.com>
2014-10-21 23:02:36 +0200

    about yatimesheet.de -> moved terms and conditions to top left in header

--- app/views/footer.blade.php
+++
@@ -1,4 +0,0 @@
-<ul class='list-inline'>
-	<li><a class='option' href='{{ url( "terms-and-conditions" ) }}'>Terms and Conditions of yatimesheet.de</a></li>
-	<li><a class='option' href='mailto:info@yatimesheet.de'>info@yatimesheet.de</a></li>
-</ul>
