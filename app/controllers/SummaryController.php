fb053046484afc89303baa265f56b78c197f56c6
Matthäus Zloch <matthaeus.zloch@gmail.com>
2014-11-19 21:45:38 +0100

    SummaryController -> changed from sql-query to view for summary by context

--- app/controllers/SummaryController.php
+++ app/controllers/SummaryController.php
@@ -49,8 +49,8 @@ class SummaryController extends BaseController
             ->join( 'contexts', 'tisheets.context_id', '=', 'contexts.id' )
             ->select( 'contexts.prefLabel', DB::raw( 'sum( tisheets.time_spent ) as total_time_spent' ) )
             ->where( 'tisheets.user_id', Auth::user()->id )
-            ->where( 'tisheets.day', '>', date( 'Y-m-d', strtotime( $period == 'week' ? 'last monday' : '-1 '. $period, $dayAsTime ) ) )
-            ->where( 'tisheets.day', '<=', $day )
+            ->where( 'tisheets.day', '>=', date( 'Y-m-d', strtotime( $period == 'week' ? 'last monday' : '-1 '. $period, $dayAsTime ) ) )
+            ->where( 'tisheets.day', '<', $day )
             ->groupBy( 'contexts.prefLabel' )
             ->get();
 
@@ -91,17 +91,12 @@ class SummaryController extends BaseController
         // total time spent
         $tts = Input::get( 'tts' );
 
-        // select day, time_spent, c.prefLabel from tisheets t join contexts c on t.context_id=c.id group by day, c.preflabel;
-        $sum = DB::table( 'tisheets' )
-            ->join( 'contexts', 'tisheets.context_id', '=', 'contexts.id' )
-            ->select( 'tisheets.day', DB::raw( 'sum( tisheets.time_spent ) as time_spent' ), 'contexts.prefLabel' )
-            ->where( 'tisheets.user_id', Auth::user()->id )
-            ->where( 'contexts.prefLabel', '#'. $context )
-            ->where( 'tisheets.day', '>', date( 'Y-m-d', strtotime( '-1 week', $dayAsTime ) ) )
-            ->where( 'tisheets.day', '<=', $day )
-            ->groupBy( 'tisheets.day' )
-            ->groupBy( 'contexts.prefLabel' )
-            ->orderBy( 'tisheets.day', 'desc' )
+        $sum = DB::table( 'summary_by_context as s' )
+            ->select( 's.day', 's.time_spent', 's.context', 's.subContext' )
+            ->where( 's.user_id', Auth::user()->id )
+            ->where( 's.context', '#'. $context )
+            ->where( 's.day', '>=', date( 'Y-m-d', strtotime( '-1 week', $dayAsTime ) ) )
+            ->where( 's.day', '<', $day )
             ->get();
 
         return View::make( 'ajax.summary-by-day-and-context' )
