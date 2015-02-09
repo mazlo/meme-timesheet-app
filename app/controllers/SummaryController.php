7b36862f8ba530ca9d1c936eaa79aeca86803f6a
Matthäus Zloch <matthaeus.zloch@gmail.com>
2015-02-09 18:32:06 +0100

    summary, made summary by context more generic -> made forWeekGroupByDaysAndContextsByContext be byContextGroupByDaysGroupByContexts;

--- app/controllers/SummaryController.php
+++ app/controllers/SummaryController.php
@@ -62,11 +62,21 @@ class SummaryController extends BaseController
 	}
 
     /**
-    *
+    *   This method handles requests for
+    *   -> show summary by contexts -> current $period -> Main Context $context.
     */
-    public function forWeekGroupByDaysAndContextsByContext( $day, $context )
+    public function byContextGroupByDaysGroupByContexts( $day, $period, $context )
     {
-        $dayAsTime = strtotime( $day );
+        $relativeDayAsTime = strtotime( $day );
+
+        if ( $period == 'week' )
+            $startDate = 'last monday';
+        else if ( $period == 'month' )
+            $startDate = 'first day of '. date( 'M', $relativeDayAsTime );
+        else if ( $period == 'year' )
+            $startDate = 'first day of Jan';
+        else
+            $startDate = 'today';
 
         // total time spent
         $tts = Input::get( 'tts' );
@@ -75,7 +85,7 @@ class SummaryController extends BaseController
             ->select( 's.day', 's.time_spent', 's.context', 's.subContext' )
             ->where( 's.user_id', Auth::user()->id )
             ->where( 's.context', '#'. $context )
-            ->where( 's.day', '>=', date( 'Y-m-d', strtotime( 'last monday', $dayAsTime ) ) )
+            ->where( 's.day', '>=', date( 'Y-m-d', strtotime( $startDate, $relativeDayAsTime ) ) )
             ->where( 's.day', '<=', $day )
             ->get();
 
