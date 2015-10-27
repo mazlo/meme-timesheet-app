d896ee16720b7bd2bd9646b1105ccc41a508f90f
Matthäus Zloch <matthaeus.zloch@gmail.com>
2015-10-27 16:58:58 +0100

    tisheet functions -> extracted method for updating quarter of time

--- public/tisheet-functions.js
+++ public/tisheet-functions.js
@@ -5,3 +5,33 @@ var runStopwatch = function( tisheet, command, startOnly )
 {
     tisheet.find( '.js-octicon-stopwatch' ).trigger( 'click', { name: command, startOnly: startOnly } );
 }
+
+/**
+*
+*/
+var updateQuarterOfTime = function ( tisheet, obj ) 
+{
+    var param = obj.callback.param;
+
+    // if value is given in minutes
+    if ( param.indexOf( 'min' ) > 0 )
+    {
+        param = param.split( 'min' )[0];
+        if ( param <= 240 )
+            tisheet.find( 'span.js-time-spent-quarter:eq('+ roundToQuarterOfHour( param ) + ')' ).click();
+        else 
+            tisheet.find( 'span.js-time-spent-quarter:eq(15)' ).click();
+    }
+    
+    // else it is expected to be given in hours
+    else
+    {
+        if ( param.indexOf( 'h' ) > 0 )
+            param = param.split( 'h' )[0];
+
+        if ( param <= 4 )
+            tisheet.find( 'span.js-time-spent-quarter:eq('+ roundToQuarterOfHour( param*60 ) + ')' ).click();
+        else
+            tisheet.find( 'span.js-time-spent-quarter:eq(15)' ).click();
+    }
+}
