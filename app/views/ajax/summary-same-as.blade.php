ee7ca8d5b9a9a47148306af8058af08c5a92a11c
Matthäus Zloch <matthaeus.zloch@gmail.com>
2016-01-08 10:03:49 +0100

    summary-same-as -> add ajax view and css formatting

---
+++ app/views/ajax/summary-same-as.blade.php
@@ -0,0 +1,17 @@
+<h3>History of Notes</h3>
+<h4>{{ $refTisheet->description }}</h4>
+
+<table cellpadding='0' cellspacing='0'>
+    <colgroup>
+        <col width='120px'>
+        <col width='500px'>
+        <col width='25px'
+    </colgroup>
+@foreach( $tisheets as $tisheet )
+    <tr>
+        <td>{{ date( 'Y-m-d', strtotime( $tisheet->day ) ) }}</td>
+        <td>{{ nl2br( $tisheet->content ) }}</td>
+        <td>{{ $tisheet->time_spent/4 }}h</td>
+    </tr>
+@endforeach
+</table>
