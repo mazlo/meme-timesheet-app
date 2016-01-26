d1f08f73787f380543adbefc8c7e9cae8e621df9
Matthäus Zloch <matthaeus.zloch@gmail.com>
2016-01-26 22:12:23 +0100

    add migration -> add column 'trashed' bool after id

---
+++ app/database/migrations/2016_01_26_205848_add_trashed_column_to_columnItems.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class AddTrashedColumnToColumnItems extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table( 'column_items', function( $table )
+		{
+			$table->boolean( 'trashed' )->default( '0' )->after( 'id' );
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table( 'ColumnItems', function( $table )
+		{
+			$table->dropColumn( 'trashed' );
+		});
+	}
+
+}
