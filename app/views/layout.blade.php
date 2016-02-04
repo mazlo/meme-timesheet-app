8170f231aea35f0044f0686e03b3fcb156fb8afb
Matthäus Zloch <matthaeus.zloch@gmail.com>
2016-02-04 17:00:41 +0100

    story, fix -> change jquery selector to capture focusin and focusout

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -264,7 +264,7 @@
         hideTisheetErrorMessages();
 	});
 
-	$jQ( document ).on( 'focusin', 'textarea.timesheet-topic', function()
+	$jQ( document ).on( 'focusin', 'div.timesheet-topic textarea', function()
 	{
 		oldTopic = $jQ( this ).val();
         
@@ -384,7 +384,7 @@
 	});
 
 	//
-	$jQ( document ).on( 'focusout', 'textarea.timesheet-topic', function()
+	$jQ( document ).on( 'focusout', 'div.timesheet-topic textarea', function()
 	{
 		var value = $jQ(this).val();
 
