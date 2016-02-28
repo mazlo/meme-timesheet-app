49510730431621bfddea33dd40eb7460f6524409
Matthäus Zloch <matthaeus.zloch@gmail.com>
2016-02-28 01:35:04 +0100

    composer.json -> add brainsocket; after composer update I needed to add some laravel update changes to users-model

--- app/models/User.php
+++ app/models/User.php
@@ -37,4 +37,19 @@ class User extends Eloquent implements UserInterface {
 	{
 		return $this->password;
 	}
+
+	public function getRememberToken()
+	{
+    		return $this->remember_token;
+	}
+
+	public function setRememberToken($value)
+	{
+    		$this->remember_token = $value;
+	}
+
+	public function getRememberTokenName()
+	{
+    		return 'remember_token';
+	} 
 }
