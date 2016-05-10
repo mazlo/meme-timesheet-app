1bc1fe38f54f4a0ed374c57d68627c371b1d9d61
Matthäus Zloch <matthaeus.zloch@gmail.com>
2016-05-11 00:24:51 +0200

    fix medkit -> show active if true

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -31,8 +31,8 @@
 		<div class='timesheet-options' style='position: absolute; left: -75px; width: 32px; text-align: center'>
 			<span class='octicon octicon-book octicon-light @if( Auth::user()->showStory )octicon-active@endif'></span>
 			<span class='octicon octicon-server octicon-light @if( Auth::user()->showColumns )octicon-active@endif'></span>
-			<span class='octicon-splitter'>&nbsp;</span>
-			<span class='ionicons ion-medkit'></span>
+			<span class='octicon-splitter'>&nbsp;</span>	<?php $sickToday = isset( $timesheet ) && $timesheet->sick ?>
+			<span class='ionicons ion-medkit @if( $sickToday )octicon-active@endif'></span>
 		</div>
 
 		<h2>ya timesheet for @if( $today == $todayForReal ) today - @endif {{ date( 'l, dS M.', $todayAsTime ) }}</h2>
