d6031a4a14fa901a80d8b22345c1830da057970e
Matthäus Zloch <matthaeus.zloch@gmail.com>
2016-06-08 16:38:34 +0200

    columns, fix -> activate column-item after creation

--- app/views/ajax/columns.blade.php
+++ app/views/ajax/columns.blade.php
@@ -7,12 +7,17 @@
             <input class='js-column-label-input' type='text' placeholder='item description' value='{{ $column->label }}' @if( $column->color ) style='background-color: {{ $column->color }}' @endif>
             <span class='octicon octicon-trashcan octicon-no-padding-left element-invisible-toggable'></span>
         </div>
+
+        {{-- list of column-items --}}
         <ul class='js-column-items'>
             @foreach( $column->items as $item )
                 @if ( !$item->trashed ) 
+
+            {{-- column-item --}}
             <li class='column-item js-column-item' id='{{ $item->id }}'>
                 <textarea class='column-item-label js-column-item-label @if ( $item->important )column-item-label-important@endif' type='text' placeholder='item description'>{{ $item->label }}</textarea>
 
+                {{-- options below the textarea --}}
                 <div class='column-item-options js-column-item-options element-hidden-toggable'>
                     <span class='octicon octicon-trashcan octicon-no-padding-left'></span>
                     <span class='ionicons ion-alert octicon-lower-padding-left'></span>
@@ -23,10 +28,16 @@
                 @endif
             @endforeach
 
-            {{-- this is empty and empty --}}
+            {{-- empty column-item, ready to be added --}}
             <li class='column-item js-column-item js-column-item-empty element-invisible-toggable'>
                 <textarea class='column-item-label js-column-item-label' type='text' placeholder='item description'></textarea>
-                <span class='octicon octicon-trashcan octicon-no-padding-left element-invisible-toggable'></span>
+                
+                {{-- options below the textarea --}}
+                <div class='column-item-options js-column-item-options element-hidden-toggable'>
+                    <span class='octicon octicon-trashcan octicon-no-padding-left'></span>
+                    <span class='ionicons ion-alert octicon-lower-padding-left'></span>
+                    <span class='ionicons ion-arrow-move octicon-lower-padding-left'></span>
+                </div>
             </li>
         </ul>
 
@@ -42,14 +53,27 @@
     @endif
 @endforeach
 
-    {{-- this is empty and empty --}}
+    {{-- this is an empty column, ready to be added --}}
     <li class='column js-column js-column-empty element-invisible-toggable' id='undefined'>
         <div class='column-label js-column-label'>
             <input class='js-column-label-input' type='text' placeholder='column label'>
             <span class='octicon octicon-trashcan octicon-no-padding-left element-invisible-toggable'></span>
         </div>
-        <ul>
-            <li class='column-item js-column-item js-column-item-empty element-invisible-toggable'><textarea class='column-item-label js-column-item-label' type='text' placeholder='item description'></textarea></li>
+
+        {{-- list of column-items --}}
+        <ul class='js-column-items'>
+
+            {{-- empty column-item, ready to be added --}}
+            <li class='column-item js-column-item js-column-item-empty element-invisible-toggable'>
+                <textarea class='column-item-label js-column-item-label' type='text' placeholder='item description'></textarea>
+                
+                {{-- options below the textarea --}}
+                <div class='column-item-options js-column-item-options element-hidden-toggable'>
+                    <span class='octicon octicon-trashcan octicon-no-padding-left'></span>
+                    <span class='ionicons ion-alert octicon-lower-padding-left'></span>
+                    <span class='ionicons ion-arrow-move octicon-lower-padding-left'></span>
+                </div>
+            </li>
         </ul>
 
         <div class='column-item-color-palette'>
